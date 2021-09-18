8b923490a0e366b34bd8dabb27e31e189816cb86
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2021-09-18 08:02:42 -0400

    Update start_server.sh script

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,4 @@
 export NODE_ENV=production
 
 forever stop server.js
-forever start server.js
-
-#forever restart server.js
-#forever stop server.js
-#forever list
+forever start -l noisecraft.log server.js
