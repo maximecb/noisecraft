9a536bb06cbb54a88b091e986b146efd308f6c46
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2021-10-05 08:49:33 -0400

    Add a bit more to tests.js

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import * as model from './public/model.js';
+import { assert } from './public/utils.js';
 
 fs.readdirSync('examples').forEach(fileName => {
     let filePath = path.join('examples', fileName);
@@ -14,7 +15,17 @@ fs.readdirSync('examples').forEach(fileName => {
     m.deserialize(data);
 
     // Test serialization
-    m.serialize();
+    let out = m.serialize();
+    assert (out.length > 0);
 });
 
-process.exit(0)
+let m = new model.Model();
+m.new();
+m.update(new model.CreateNode('AudioOut', 0, 0));
+assert (m.hasNode('AudioOut'));
+m.serialize();
+
+// TODO: test undo/redo
+
+// TODO: test SetParam
+// May need getNodesOfType(nodeType)
