56264dcdccee3cbdedbeebd1731a2a1aec2f8097
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2021-11-14 23:35:05 -0500

    Progress on schema converter script

--- converter.js
+++ converter.js
@@ -5,17 +5,40 @@
 import fs from 'fs';
 import path from 'path';
 import sqlite3 from 'sqlite3';
-import { assert } from './public/utils.js';
+import { assert, treeCopy } from './public/utils.js';
 import * as model from './public/model.js';
 import { compile } from './public/compiler.js';
 
-// TODO: take inspiration from validation code
-function convert(project)
+function convertProject(project)
 {
+    assert (project instanceof Object);
+
+    // Validate each individual node
+    for (let nodeId in project.nodes)
+    {
+        assert (typeof nodeId === 'string');
+        let node = project.nodes[nodeId];
+        project.nodes[nodeId] = convertNode(node);
+    }
+
+    return project;
 }
 
 function convertNode(node)
 {
+    let schema = model.NODE_SCHEMA[node.type];
+
+    if (!node.inNames)
+    {
+        node.inNames = schema.ins.map(s => s.name);
+    }
+
+    if (!node.outNames)
+    {
+        node.outNames = schema.outs.map(n => n);
+    }
+
+    return node;
 }
 
 //===========================================================================
@@ -26,20 +49,79 @@ fs.readdirSync('examples').forEach(fileName =>
     // Read the example file
     let filePath = path.join('examples', fileName);
     console.log(filePath);
-    let data = fs.readFileSync(filePath, 'utf8')
+    let inData = fs.readFileSync(filePath, 'utf8')
+    let project = JSON.parse(inData);
 
+    // Convert the project
+    project = convertProject(project);
 
-
-
-
+    let outData = JSON.stringify(project);
+    //fs.writeFileSync(filePath, outData, { encoding: "utf8" });
 });
 
-/*
+
+//===========================================================================
+
 // Connect to the database
-let db = new sqlite3.Database('./database.db', (err) => 
+async function connect()
 {
-    if (err)
-        throw err;
-    console.log('Connected to the database');
-});
-*/
+    return new Promise((resolve, reject) => {
+        let db = new sqlite3.Database('./database.db', (err) =>
+        {
+            if (err)
+                reject();
+
+            console.log('Connected to the database');
+            resolve(db);
+        })
+    })
+}
+
+// Get the maximum project id
+async function getMaxId()
+{
+    return new Promise((resolve, reject) => {
+        db.get(
+            'SELECT MAX(id) as maxId FROM projects',
+            [],
+            function (err, row)
+            {
+                if (err)
+                    reject();
+                resolve(row.maxId);
+            }
+        );
+    });
+}
+
+// TODO: getProjectData()
+
+
+
+
+
+let db = await connect();
+console.log(db);
+
+let maxProjectId = await getMaxId();
+assert (typeof maxProjectId == 'number');
+console.log(`maxProjectId=${maxProjectId}`);
+
+// For each project id
+for (let projectId = 0; projectId <= maxProjectId; ++projectId)
+{
+    console.log(`processing projectId=${projectId}`);
+
+
+
+    
+
+}
+
+
+
+
+
+
+
+db.close();
