b76588cc5005b88e6ee73370ebdb602847255fa2
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2026-09-06 10:42:48 -0400

    Update script

--- scripts/download_db.sh
+++ scripts/download_db.sh
@@ -8,4 +8,7 @@ ssh "${SERVER_ADDR}" "pm2 stop noisecraft"
 DATE=$(date +"%Y-%m-%d")
 rsync -avz "${SERVER_ADDR}:noisecraft/deploy/database.db" "${DATE}-database.db"
 
+# Keep one copy in dropbox
+cp "${DATE}-database.db" /Users/maximecb/Dropbox/Projects/noisecraft/database.db
+
 ssh "${SERVER_ADDR}" "pm2 start noisecraft"
